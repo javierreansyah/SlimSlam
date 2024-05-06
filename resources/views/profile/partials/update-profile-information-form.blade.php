a4c282d689384406b9e201751d85e0ee39759051
Javier <javierreansyah@gmail.com>
2024-05-07 02:26:36 +0700

    first commit

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,10 +1,10 @@
 <section>
     <header>
-        <h2 class="text-lg font-medium text-gray-900">
+        <h2 class="text-lg font-medium text-primary">
             {{ __('Profile Information') }}
         </h2>
 
-        <p class="mt-1 text-sm text-gray-600">
+        <p class="mt-1 text-sm text-foreground">
             {{ __("Update your account's profile information and email address.") }}
         </p>
     </header>
