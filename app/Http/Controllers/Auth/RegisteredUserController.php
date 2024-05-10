6948135123ba6cf1c6185a9b45b2d31b554e8a18
Javier <javierreansyah@gmail.com>
2024-05-10 17:34:35 +0700

    fixed first measurement record when registering

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
+use App\Models\UserMeasurement;
 
 class RegisteredUserController extends Controller
 {
@@ -49,7 +50,16 @@ class RegisteredUserController extends Controller
             'birth_date' => $request->birth_date,
         ]);
 
+        $measurement = UserMeasurement::create([
+            'user_id' => $user->id,
+            'weight' => $request->weight,
+            'height' => $request->height,
+            'bmi' => $request->weight / (($request->height / 100) ** 2),
+            'recorded_at' => now(),
+        ]);
+
         event(new Registered($user));
+        event(new Registered($measurement));
 
         Auth::login($user);
 
