8e0a3bf055e78ecdb579fe6d4e142d35182ebb6b
Haikal Thoriq Athaya <haikalthrq@gmail.com>
2024-05-15 14:42:46 +0700

    Added card design to 'About' section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,11 +31,15 @@
             <main class="">
                 <div class="flex h-screen bg-cover bg-center" style="background-image: url('storage/home/home-bg.jpg')">
                     <div class="mt-40 mx-auto px-10 leading-tight text-white">
-                        <p class="text-4xl md:text-2xl lg:text-5xl xl:text-7xl font-black leading-tight">Empowering <br> Wellness <br> Inspiring Fitness</p>
-                        <p class="text-xl text-muted-foreground max-w-sm pt-4 font-normal leading-tight">Slam into Fitness with <span class="text-primary font-semibold">SlimSlam</span></p>
+                        <p class="text-4xl md:text-2xl lg:text-5xl xl:text-7xl font-black leading-tight">Empowering <br>
+                            Wellness <br> Inspiring Fitness</p>
+                        <p class="text-xl text-muted-foreground max-w-sm pt-4 font-normal leading-tight">Slam into
+                            Fitness with <span class="text-primary font-semibold">SlimSlam</span></p>
 
                         <div class="mt-10">
-                            <a href="https://www.linkedin.com/in/javierreansyah/" class="text-xl rounded bg-primary px-5 py-3 font-medium text-background hover:bg-muted" target="_blank">Join Now</a>
+                            <a href="https://www.linkedin.com/in/javierreansyah/"
+                                class="text-xl rounded bg-primary px-5 py-3 font-medium text-background hover:bg-muted"
+                                target="_blank">Join Now</a>
                         </div>
                     </div>
 
@@ -47,40 +51,67 @@
                 <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                     <div class="bg-background shadow-lg rounded-lg p-8">
                         <h1 class="text-4xl font-bold text-primary mb-6 justify-start">About SlimSlam</h1>
-                        <p class="text-lg text-white mb-4">
-                            Welcome to SlimSlam, your ultimate partner in achieving a healthier and more active lifestyle. Our platform offers a range of features tailored to help you reach your fitness goals with ease and enjoyment. Here's what you can expect from SlimSlam:
-                        </p>
-                        
-                        <h2 class="text-3xl font-semibold text-primary mt-8 mb-4">Personalized Workouts</h2>
-                        <p class="text-lg text-white mb-4">
-                            At SlimSlam, we understand that fitness is not a one-size-fits-all journey. That's why we offer a variety of workouts that cater to different hobbies and interests. Whether you're into yoga, gym workouts, or other fitness activities, we've got you covered. Our workout plans are designed to be flexible and enjoyable, ensuring that you stay motivated and engaged.
-                        </p>
-                        <ul class="list-disc list-inside text-lg text-white mb-4">
-                            <li><strong>Yoga:</strong> Find your inner peace and improve your flexibility with our range of yoga sessions, suitable for all levels.</li>
-                            <li><strong>Gym Workouts:</strong> Build strength and endurance with our comprehensive gym workout plans, from beginner to advanced levels.</li>
-                            <li><strong>Specialized Activities:</strong> Enjoy other fitness activities tailored to your hobbies and preferences.</li>
-                        </ul>
-            
-                        <h2 class="text-3xl font-semibold text-primary mt-8 mb-4">BMI and Weight Tracking</h2>
-                        <p class="text-lg text-white mb-4">
-                            Stay on top of your health with our advanced tracking features. SlimSlam allows you to monitor your Body Mass Index (BMI) and weight over time, helping you understand your progress and adjust your fitness plan accordingly. Our tracking tools are easy to use and provide valuable insights into your health journey.
-                        </p>
-                        <ul class="list-disc list-inside text-lg text-white mb-4">
-                            <li><strong>BMI Tracking:</strong> Calculate and track your BMI to understand your body composition and health status.</li>
-                            <li><strong>Weight Monitoring:</strong> Record your weight regularly and see how it changes over time. We'll remind you to update your weight at specific intervals to ensure you're staying on track.</li>
-                        </ul>
-            
-                        <h2 class="text-3xl font-semibold text-primary mt-8 mb-4">Regular Progress Check-ins</h2>
-                        <p class="text-lg text-white mb-4">
-                            Consistency is key to achieving your fitness goals. SlimSlam provides regular check-ins to help you stay accountable and motivated. Our system will remind you to check your BMI and weight at regular intervals, allowing you to see your progress and make necessary adjustments to your fitness plan.
-                        </p>
-                        <ul class="list-disc list-inside text-lg text-white mb-4">
-                            <li><strong>Scheduled Check-ins:</strong> Receive reminders to update your BMI and weight, keeping you on track with your health journey.</li>
-                            <li><strong>Progress Reports:</strong> Get detailed reports on your progress, helping you visualize your achievements and areas for improvement.</li>
-                        </ul>
-            
-                        <p class="text-lg text-white mt-8">
-                            Join SlimSlam today and start your journey towards a healthier, happier you. With our personalized workouts, comprehensive tracking tools, and regular check-ins, achieving your fitness goals has never been easier or more enjoyable. Let's transform your health together!
+                        <div class="bg-card rounded-lg">
+                            <div class="p-10">
+                                <h2 class="text-3xl font-semibold text-primary mb-4">Personalized Workouts</h2>
+                                <p class="text-lg text-white mb-4">
+                                    At SlimSlam we understand that fitness is not a one-size-fits-all journey. That's
+                                    why we offer a variety of workouts that cater to different hobbies and interests.
+                                    Our workout plans are designed to be flexible and enjoyable, ensuring that you stay
+                                    motivated and engaged.
+                                </p>
+                                <ul class="list-disc list-inside text-lg text-white mb-4">
+                                    <li><strong>Yoga:</strong> Find your inner peace and improve your flexibility with
+                                        our range of yoga sessions, suitable for all levels.</li>
+                                    <li><strong>Gym Workouts:</strong> Build strength and endurance with our
+                                        comprehensive gym workout plans, from beginner to advanced levels.</li>
+                                    <li><strong>Specialized Activities:</strong> Enjoy other fitness activities tailored
+                                        to your hobbies and preferences.</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="bg-card rounded-lg mt-8">
+                            <div class="p-10">
+                                <h2 class="text-3xl font-semibold text-primary mb-4">BMI and Weight Tracking</h2>
+                                <p class="text-lg text-white mb-4">
+                                    Stay on top of your health with our advanced tracking features. SlimSlam allows you to
+                                    monitor your Body Mass Index (BMI) and weight over time, helping you understand your
+                                    progress and adjust your fitness plan accordingly. Our tracking tools are easy to use and
+                                    provide valuable insights into your health journey.
+                                </p>
+                                <ul class="list-disc list-inside text-lg text-white mb-4">
+                                    <li><strong>BMI Tracking:</strong> Calculate and track your BMI to understand your body
+                                        composition and health status.</li>
+                                    <li><strong>Weight Monitoring:</strong> Record your weight regularly and see how it changes
+                                        over time. We'll remind you to update your weight at specific intervals to ensure you're
+                                        staying on track.</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="bg-card rounded-lg mt-8">
+                            <div class="p-10">
+                                <h2 class="text-3xl font-semibold text-primary mb-4">Regular Progress Check-ins</h2>
+                                <p class="text-lg text-white mb-4">
+                                    Consistency is key to achieving your fitness goals. SlimSlam provides regular check-ins to
+                                    help you stay accountable and motivated. Our system will remind you to check your BMI and
+                                    weight at regular intervals, allowing you to see your progress and make necessary
+                                    adjustments to your fitness plan.
+                                </p>
+                                <ul class="list-disc list-inside text-lg text-white mb-4">
+                                    <li><strong>Scheduled Check-ins:</strong> Receive reminders to update your BMI and weight,
+                                        keeping you on track with your health journey.</li>
+                                    <li><strong>Progress Reports:</strong> Get detailed reports on your progress, helping you
+                                        visualize your achievements and areas for improvement.</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <p class="text-lg text-white text-justify mt-8">
+                            Join SlimSlam today and start your journey towards a healthier, happier you. With our
+                            personalized workouts, comprehensive tracking tools, and regular check-ins, achieving your
+                            fitness goals has never been easier or more enjoyable. Let's transform your health with us!
                         </p>
                     </div>
                 </div>
